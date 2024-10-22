b3bc0b8bc7c1e4da339900833432cd80f4ad4da3
Dorramires <math.dorramires@gmail.com>
2024-10-22 09:08:58 -0300

    prisma att

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -51,12 +51,12 @@ export const authOptions: NextAuthOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "email", placeholder: "exemplo@exemplo.com" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+        const user = { id: "1", name: "Joao", email: "test@test.com" }
   
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
